e5ffd641fe8c40f8d1fb01c3d21a38fc028df6b7
Firas Ghanem <ghanem.firas30@gmail.com>
2017-08-09 17:05:14 +0300

    day1

--- Mini.js
+++ Mini.js
@@ -2,14 +2,39 @@ function change() {
 	$('body').css("background-color",'lightblue')
 }
 
-var results = {
-	0: 0,
-	5: 1,
-	10: 2,
-	15: 3,
-	20: 4,
-	25: 5,
-	30: 6
+// var results = {
+// 	0: 0,
+// 	5: 1,
+// 	10: 2,
+// 	15: 3,
+// 	20: 4,
+// 	25: 5,
+// 	30: 6
+// }
+var counter=0;
+
+
+function result(){
+	var talk = ["You've field Humanity, you scored nothing!", "You scored "];
+	var i=1
+	 while(i<=6)
+{
+	if($("input[name="+i+"]:checked").val()==="correct")
+	{
+		counter+=5;
+		i++
+	
+	}
+	else{
+		i++
+	}
 }
 
-var talk = ["You've field Humanity, you scored nothing!", "You scored"+Object.keys(results)[key]]
+if(counter===0){
+	alert(talk[0])
+}else{
+	alert(talk[1]+counter) 
+}
+
+
+}

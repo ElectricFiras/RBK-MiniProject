4db9a8aa9a962b337cbdc45e03d0f1aac37b0ac4
Firas Ghanem <ghanem.firas30@gmail.com>
2017-08-09 14:30:22 +0300

    Second save-Base Model

---
+++ Mini.js
@@ -0,0 +1,15 @@
+function change() {
+	$('body').css("background-color",'lightblue')
+}
+
+var results = {
+	0: 0,
+	5: 1,
+	10: 2,
+	15: 3,
+	20: 4,
+	25: 5,
+	30: 6
+}
+
+var talk = ["You've field Humanity, you scored nothing!", "You scored"+Object.keys(results)[key]]

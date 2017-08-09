a5fb1de4593d83a897696e661332a105d3ade9cc
Firas Ghanem <ghanem.firas30@gmail.com>
2017-08-09 22:47:24 +0300

    Semi Final

--- Mini.js
+++ Mini.js
@@ -1,5 +1,7 @@
-function change() {
-	$('body').css("background-color",'lightblue')
+function change() { 
+	if (document.body.style.backgroundColor === ""){
+	$('body').css("background-color",'lightblue')}
+	else $('body').css("background-color", '')
 }
 
 // var results = {
@@ -37,4 +39,27 @@ if(counter===0){
 }
 
 
+}
+
+var back = ['"Apollo 11.jpg"','"Apollo 11-1.jpg"', '"Apollo 11-2.jpg"','"Apollo 11-3.jpg"','"Apollo 11-4.jpg"','"Apollo 11-5.jpg"']
+var mgc = 0
+function next(){
+	mgc++
+	if (mgc > 5){
+		mgc = 0
+		$('#bodqz').css("background-image", 'url('+back[mgc]+')')
+		
+	} else{
+	$('#bodqz').css("background-image", 'url('+back[mgc]+')')
+	}
+}
+
+function previous(){
+	mgc--
+	if (mgc < 1){
+		mgc = 5
+		$('#bodqz').css("background-image", 'url('+back[mgc]+')')
+	} else{
+	$('#bodqz').css("background-image", 'url('+back[mgc]+')')
+	}
 }
